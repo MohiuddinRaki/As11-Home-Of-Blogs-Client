8ce428ab2683c993647de1ba222fa852f7dedf98
Rakib <mohammadmohiuddin3490@gmai.com>
2023-11-08 19:00:08 +0600

    filter by category in all blogs page

--- src/pages/PageInNavbar/AllBlogs.jsx
+++ src/pages/PageInNavbar/AllBlogs.jsx
@@ -12,20 +12,63 @@ const AllBlogs = () => {
     );
     setFilterBlogs(filteringBlog);
   };
+  const handleSearchCaetegory = (event) => {
+    const filteringBlogs = filterBlogs.filter((blog) =>
+      blog.category.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    setFilterBlogs(filteringBlogs);
+  };
 
   return (
     <>
       <div>
         <div>
-          <div className="text-center my-5">
-            <input
-              className="text-xl border font-medium text-center py-2"
-              type="text"
-              placeholder="search by title..."
-              onChange={handleSearch}
-            />
+          <div className=" my-5 flex justify-center gap-5">
+            <div>
+              <form onChange={handleSearchCaetegory}>
+                <select type="text" className="input input-bordered">
+                  <option value={recentBlog}>all</option>
+                  <option value="Travel">Travel</option>
+                  <option value="Health & Wellness">Health & Wellness</option>
+                  <option value="Food & Cooking">Food & Cooking</option>
+                  <option value="Finance">Finance</option>
+                  <option value="Fitness">Fitness</option>
+                  <option value="Sports and Recreatio">
+                    Sports and Recreatio
+                  </option>
+                  <option value="Science and Technology">
+                    Science and Technology
+                  </option>
+                  <option value="Gardening and Landscaping">
+                    Gardening and Landscaping
+                  </option>
+                  <option value="Pet Care and Tips">Pet Care and Tips</option>
+                  <option value="Art and Creativity">Art and Creativity</option>
+                  <option value="Environmental Sustainability">
+                    Environmental Sustainability
+                  </option>
+                  <option value="Career Development">Career Development</option>
+                  <option value="Movie and Entertainment">
+                    Movie and Entertainment
+                  </option>
+                  <option value="Book Reviews">Book Reviews</option>
+                  <option value="Outdoor Adventures">Outdoor Adventures</option>
+                  <option value="Parenting Tips">Parenting Tips</option>
+                  <option value="Home Decor & DIY">Home Decor & DIY</option>
+                  <option value="Fashion Forward">Fashion Forward</option>
+                  <option value="Lifestyle Hacks">Lifestyle Hacks</option>
+                </select>
+              </form>
+            </div>
+            <div>
+              <input
+                className="text-xl input input-bordered font-medium text-center py-2"
+                type="text"
+                placeholder="search by title..."
+                onChange={handleSearch}
+              />
+            </div>
           </div>
-
           <div className="container mx-auto p-8 md:p-16 lg:p-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-20">
             {filterBlogs.map((blog, idx) => (
               <AllBlog key={idx} blog={blog}></AllBlog>
